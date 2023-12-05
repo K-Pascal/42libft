f0b5c0a2c7c65de3a86033855d5a820a3fe3c19a
Pascal <152603632+K-Pascal@users.noreply.github.com>
2023-12-05 20:10:03 +0100

    Added ft_calloc

---
+++ ft_calloc.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pnguyen- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/06 17:58:35 by pnguyen-          #+#    #+#             */
+/*   Updated: 2023/11/06 18:22:42 by pnguyen-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+#include "libft.h"
+
+void	*ft_calloc(unsigned int nmemb, unsigned int size)
+{
+	void			*array;
+	unsigned long	len;
+
+	len = nmemb * size;
+	if (len >= 0b11111111111111111111111111111111)
+		return (NULL);
+	array = malloc(nmemb * size);
+	if (array == NULL)
+		return (NULL);
+	ft_bzero(array, nmemb * size);
+	return (array);
+}
