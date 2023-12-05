47d574a75ec4bf3e1da170fb87acf08788d77f54
Pascal <152603632+K-Pascal@users.noreply.github.com>
2023-12-05 20:10:03 +0100

    Added ft_strlcat.c file

---
+++ ft_strlcat.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pnguyen- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/06 13:32:23 by pnguyen-          #+#    #+#             */
+/*   Updated: 2023/11/06 13:59:58 by pnguyen-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+unsigned int	ft_strlen(const char s[])
+{
+	unsigned int	i;
+
+	i = 0;
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
+
+unsigned int	ft_strlcat(char dst[], const char src[], unsigned int size)
+{
+	unsigned int	len_d;
+	unsigned int	j;
+
+	len_d = ft_strlen(dst);
+	if (len_d < size)
+	{
+		j = 0;
+		while (src[j] != '\0')
+		{
+			if (j + len_d + 1 >= size)
+				break ;
+			dst[len_d + j] = src[j];
+			j++;
+		}
+		dst[len_d + j] = '\0';
+		return (len_d + ft_strlen(src));
+	}
+	return (size);
+}
