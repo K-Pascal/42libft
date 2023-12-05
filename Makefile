ad029ec3fc1e3ca87a6bb2a3e250b99a72c5d4e5
Pascal <152603632+K-Pascal@users.noreply.github.com>
2023-12-05 20:09:03 +0100

    Added ft_tolower.c + command test

--- Makefile
+++ Makefile
@@ -1,5 +1,6 @@
 SOURCES := ft_isalpha.c ft_isdigit.c ft_isalnum.c ft_isascii.c ft_isprint.c ft_strlen.c ft_memset.c \
-		   ft_bzero.c ft_memcpy.c ft_memmove.c ft_strlcpy.c ft_strlcat.c ft_toupper.c
+		   ft_bzero.c ft_memcpy.c ft_memmove.c ft_strlcpy.c ft_strlcat.c ft_toupper.c ft_tolower.c \
+
 OBJECTS := $(SOURCES:.c=.o)
 
 CC := cc
@@ -13,11 +14,10 @@ all: $(NAME)
 $(NAME): $(OBJECTS)
 	ar -rcs $@ $^
 
-%.o: %.c libft.h
-	$(CC) $(CFLAGS) -I . -c $< -o $@
+%.o: %.c
+	$(CC) $(CFLAGS) -c $< -o $@
 
 .PHONY: clean fclean re
-
 clean:
 	rm -f $(OBJECTS)
 
@@ -25,3 +25,10 @@ fclean: clean
 	rm -f $(NAME)
 
 re: fclean all
+
+.PHONY: norm test
+norm:
+	norminette -R CheckForbiddenSourceHeader -R CheckDefine
+
+test: $(NAME) libft.h
+	$(CC) $(FLAGS) -I. -L. -lft main.c
---
+++ libft.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pnguyen- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/06 14:29:18 by pnguyen-          #+#    #+#             */
+/*   Updated: 2023/11/06 14:35:04 by pnguyen-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+int				ft_isalpha(int c);
+int				ft_isdigit(int c);
+int				ft_isalnum(int c);
+int				ft_isascii(int c);
+int				ft_isprint(int c);
+unsigned int	ft_strlen(const char s[]);
+void			*ft_memset(void *s, int c, unsigned int n);
+void			ft_bzero(void *s, unsigned int n);
+void			*ft_memcpy(void *dest, const void *src, unsigned int n);
+void			*ft_memmove(void *dest, const void *src, unsigned int n);
+unsigned int	ft_strlcpy(char dst[], const char src[], unsigned int size);
+unsigned int 	ft_strlcat(char dst[], const char src[], unsigned int size);
+int				ft_toupper(int c);
+int				ft_tolower(int c);
+
+#endif
