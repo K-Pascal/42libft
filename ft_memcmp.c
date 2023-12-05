4238ecb6279ecfbacfa611fe0025d69fe63caea0
Pascal <152603632+K-Pascal@users.noreply.github.com>
2023-12-05 20:10:03 +0100

    Added ft_memcmp

---
+++ ft_memcmp.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pnguyen- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/06 17:01:00 by pnguyen-          #+#    #+#             */
+/*   Updated: 2023/11/06 17:07:09 by pnguyen-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_memcmp(const void *s1, const void *s2, unsigned int n)
+{
+	unsigned int	i;
+	unsigned char	c1;
+	unsigned char	c2;
+
+	if (n == 0)
+		return (0);
+	i = 0;
+	while (i < n)
+	{
+		c1 = *(unsigned char *)(s1 + i * sizeof(unsigned char));
+		c2 = *(unsigned char *)(s2 + i * sizeof(unsigned char));
+		if (c1 != c2)
+			break ;
+		i++;
+	}
+	return (c1 - c2);
+}
