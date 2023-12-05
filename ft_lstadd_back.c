57e4f822e99b3d8777327c2f2f66a27c052b2c52
Pascal <152603632+K-Pascal@users.noreply.github.com>
2023-12-05 20:09:03 +0100

    42Normed

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -6,7 +6,7 @@
 /*   By: pnguyen- <pnguyen-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/09 16:28:42 by pnguyen-          #+#    #+#             */
-/*   Updated: 2023/11/10 13:24:17 by pnguyen-         ###   ########.fr       */
+/*   Updated: 2023/11/10 15:33:57 by pnguyen-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,10 @@
 
 void	ft_lstadd_back(t_list **lst, t_list *new)
 {
+	t_list	*last;
+
 	if (lst == 0)
 		return ;
-	t_list	*last;
 	last = ft_lstlast(*lst);
 	last->next = new;
 }
