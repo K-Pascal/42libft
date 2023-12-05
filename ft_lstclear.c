20ef07cddd373cfc46430bb39c2cc14209e02192
Pascal <152603632+K-Pascal@users.noreply.github.com>
2023-12-05 20:10:03 +0100

    ft_lstclear() now functions properly

--- ft_lstclear.c
+++ ft_lstclear.c
@@ -6,7 +6,7 @@
 /*   By: pnguyen- <pnguyen-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/09 17:56:36 by pnguyen-          #+#    #+#             */
-/*   Updated: 2023/11/10 15:30:57 by pnguyen-         ###   ########.fr       */
+/*   Updated: 2023/11/10 18:57:03 by pnguyen-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,17 @@
 
 void	ft_lstclear(t_list **lst, void (*del)(void *))
 {
-	t_list	*last;
+	t_list	*temp;
 
 	if (lst == NULL || del == NULL)
 		return ;
-	last = ft_lstlast(*lst);
-	while (last != NULL)
+	while (*lst != NULL)
 	{
-		del(last->content);
-		free(last);
-		last = ft_lstlast(*lst);
+		del((*lst)->content);
+		temp = (*lst)->next;
+		free(*lst);
+		*lst = NULL;
+		*lst = temp;
 	}
 	*lst = NULL;
 }
