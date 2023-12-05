e396a572d3e85ffa9d17293572ef092f5faed988
Pascal <152603632+K-Pascal@users.noreply.github.com>
2023-12-05 20:10:04 +0100

    some micro optimizations

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: pnguyen- <pnguyen-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/06 13:14:24 by pnguyen-          #+#    #+#             */
-/*   Updated: 2023/11/11 16:23:15 by pnguyen-         ###   ########.fr       */
+/*   Updated: 2023/12/03 18:34:03 by pnguyen-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,16 @@
 
 size_t	ft_strlcpy(char dst[], const char src[], size_t size)
 {
-	size_t	i;
+	size_t	len_s;
 
-	if (size > 0)
+	len_s = 0;
+	while (*src != '\0')
 	{
-		i = 0;
-		while (src[i] != '\0')
-		{
-			if (i >= size - 1)
-				break ;
-			dst[i] = src[i];
-			i++;
-		}
-		dst[i] = '\0';
+		if (++len_s < size)
+			*(dst++) = *src;
+		src++;
 	}
-	return (ft_strlen(src));
+	if (size != 0)
+		*dst = '\0';
+	return (len_s);
 }
