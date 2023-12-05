7de20f8d54d0ed3db431f8667da8eef823e40760
Pascal <152603632+K-Pascal@users.noreply.github.com>
2023-12-05 20:10:04 +0100

    ft_strtrim() now need only 2 new functions

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: pnguyen- <pnguyen-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/07 13:54:12 by pnguyen-          #+#    #+#             */
-/*   Updated: 2023/11/12 18:12:06 by pnguyen-         ###   ########.fr       */
+/*   Updated: 2023/12/03 18:52:00 by pnguyen-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,77 +14,31 @@
 
 #include "libft.h"
 
-typedef struct s_pos
+static char	*my_strndup(char const src[], size_t n)
 {
-	int	begin;
-	int	end;
-}	t_pos;
+	char	*dest;
 
-static int	in_set(char const c, char const set[])
-{
-	int	i;
-
-	i = 0;
-	while (set[i] != '\0')
-	{
-		if (set[i] == c)
-			return (1);
-		i++;
-	}
-	return (0);
-}
-
-static t_pos	delim_aftertrim(char const s1[], char const set[])
-{
-	t_pos	delim;
-
-	delim.begin = 0;
-	while (s1[delim.begin] != '\0')
-	{
-		if (!in_set(s1[delim.begin], set))
-			break ;
-		delim.begin++;
-	}
-	delim.end = ft_strlen(s1) - 1;
-	while (delim.end >= 0)
-	{
-		if (!in_set(s1[delim.end], set))
-			break ;
-		delim.end--;
-	}
-	return (delim);
-}
-
-static char	*ft_strndup_delim(char const s1[], t_pos delim)
-{
-	char	*str;
-	int		i;
-
-	if (delim.begin > delim.end)
-	{
-		str = malloc(1 * sizeof(char));
-		str[0] = '\0';
-		return (str);
-	}
-	str = malloc((delim.end - delim.begin + 2) * sizeof(char));
-	if (str == NULL)
+	dest = ft_calloc(n + 1, sizeof(char));
+	if (dest == NULL)
 		return (NULL);
-	i = 0;
-	while (delim.begin + i <= delim.end)
-	{
-		str[i] = s1[delim.begin + i];
-		i++;
-	}
-	str[i] = '\0';
-	return (str);
+	ft_strlcpy(dest, src, n + 1);
+	return (dest);
 }
 
 char	*ft_strtrim(char const s1[], char const set[])
 {
-	char	*str;
-	t_pos	delim;
+	char		*dest;
+	char const	*start;
 
-	delim = delim_aftertrim(s1, set);
-	str = ft_strndup_delim(s1, delim);
-	return (str);
+	while (*s1 != '\0' && ft_strchr(set, *s1) != NULL)
+		s1++;
+	start = s1;
+	while (*s1 != '\0')
+		s1++;
+	s1--;
+	while (start < s1 && ft_strchr(set, *s1) != NULL)
+		s1--;
+	s1++;
+	dest = my_strndup(start, s1 - start);
+	return (dest);
 }
