e3e209e5de0e401ffe74053172e9991eed1081ff
Pascal <152603632+K-Pascal@users.noreply.github.com>
2023-12-05 20:09:03 +0100

    Functions now check if parameters are NULL

--- ft_lstadd_front.c
+++ ft_lstadd_front.c
@@ -6,7 +6,7 @@
 /*   By: pnguyen- <pnguyen-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/08 17:23:52 by pnguyen-          #+#    #+#             */
-/*   Updated: 2023/11/09 19:10:03 by pnguyen-         ###   ########.fr       */
+/*   Updated: 2023/11/10 13:18:31 by pnguyen-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 
 void	ft_lstadd_front(t_list **lst, t_list *new)
 {
+	if (lst == 0 || new == 0)
+		return ;
 	t_list	*first;
 	first = *lst;
 	new->next = first;
