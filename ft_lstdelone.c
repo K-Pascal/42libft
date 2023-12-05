7bc5689de8d3606ae83c85778e11d2fd0a1664d9
Pascal <152603632+K-Pascal@users.noreply.github.com>
2023-12-05 20:10:03 +0100

    Added ft_lstdelone

---
+++ ft_lstdelone.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstdelone.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pnguyen- <pnguyen-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/09 14:10:30 by pnguyen-          #+#    #+#             */
+/*   Updated: 2023/11/10 13:16:38 by pnguyen-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+#include "libft.h"
+
+void	ft_lstdelone(t_list *lst, void (*del)(void *))
+{
+	if (lst == NULL || del == NULL)
+		return ;
+	del(lst->content);
+	free(lst);
+}
