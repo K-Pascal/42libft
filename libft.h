857c395b3998f7e8a3369b084ae37baaa934ba41
Pascal <152603632+K-Pascal@users.noreply.github.com>
2023-12-05 20:10:03 +0100

    Added ft_lstsize

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: pnguyen- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/06 14:29:18 by pnguyen-          #+#    #+#             */
-/*   Updated: 2023/11/08 18:04:37 by pnguyen-         ###   ########.fr       */
+/*   Updated: 2023/11/08 19:17:41 by pnguyen-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,4 +56,5 @@ void			ft_putnbr_fd(int n, int fd);
 
 t_list			*ft_lstnew(void *content);
 void			ft_lstadd_front(t_list **lst, t_list *new);
+int				ft_lstsize(t_list *lst);
 #endif
