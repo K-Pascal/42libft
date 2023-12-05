254eadbaeb3ca26435c2db8ffdbefca9b4de9229
Pascal <152603632+K-Pascal@users.noreply.github.com>
2023-12-05 20:10:03 +0100

    Added ft_putnbr_fd

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: pnguyen- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/06 14:29:18 by pnguyen-          #+#    #+#             */
-/*   Updated: 2023/11/08 15:40:35 by pnguyen-         ###   ########.fr       */
+/*   Updated: 2023/11/08 17:04:08 by pnguyen-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,5 +46,6 @@ void			ft_striteri(char s[], void (*f)(unsigned int, char *));
 void			ft_putchar_fd(char c, int fd);
 void			ft_putstr_fd(char s[], int fd);
 void			ft_putendl_fd(char s[], int fd);
+void			ft_putnbr_fd(int n, int fd);
 
 #endif
