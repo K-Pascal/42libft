73f4a2b4465de552bfe6d61a4fd1daef1667e2e7
Pascal <152603632+K-Pascal@users.noreply.github.com>
2023-12-05 20:10:03 +0100

    Protected ft_substr() against invalid arguments

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: pnguyen- <pnguyen-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/07 11:24:18 by pnguyen-          #+#    #+#             */
-/*   Updated: 2023/11/09 19:18:58 by pnguyen-         ###   ########.fr       */
+/*   Updated: 2023/11/10 17:41:50 by pnguyen-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,17 @@
 
 char	*ft_substr(char const *s, unsigned int start, unsigned int len)
 {
-	char	*str;
+	char			*str;
+	unsigned int	len_s;
 
-	if (s == NULL)
+	len_s = ft_strlen(s);
+	if (s == NULL || start >= len_s) 
 		return (NULL);
+	if (start + len > len_s)
+		len = len_s - start;
 	str = malloc((len + 1) * sizeof(char));
 	if (str == NULL)
 		return (NULL);
-	ft_strlcpy(str, &s[start], sizeof(str));
+	ft_strlcpy(str, &s[start], len + 1);
 	return (str);
 }
