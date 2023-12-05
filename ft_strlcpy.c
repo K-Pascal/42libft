ef65100b4dfaf96ceba90e024f7f9baf20c62ab0
Pascal <152603632+K-Pascal@users.noreply.github.com>
2023-12-05 20:10:03 +0100

    Fixed return value incorrect

---
+++ ft_strlcpy.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pnguyen- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/06 13:14:24 by pnguyen-          #+#    #+#             */
+/*   Updated: 2023/11/06 13:39:23 by pnguyen-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+static int		ft_strlen(const char s[])
+{
+	unsigned int	i;
+
+	i = 0;
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
+
+unsigned int	ft_strlcpy(char dst[], const char src[], unsigned int size)
+{
+	unsigned int	i;
+
+	if (size > 0)
+	{
+		i = 0;
+		while (src[i] != '\0')
+		{
+			if (i < size - 1)
+				break ;
+			dst[i] = src[i];
+			i++;
+		}
+		dst[i] = '\0';
+	}
+	return (ft_strlen(src));
+}
