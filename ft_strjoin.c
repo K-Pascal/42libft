176ead2ef9a714e63d76815a7231baba61fc6784
Pascal <152603632+K-Pascal@users.noreply.github.com>
2023-12-05 20:10:03 +0100

    ft_strjoin() optimized by using ft_strlcpy instead of ft_strlcat

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: pnguyen- <pnguyen-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/07 11:39:17 by pnguyen-          #+#    #+#             */
-/*   Updated: 2023/11/12 18:10:37 by pnguyen-         ###   ########.fr       */
+/*   Updated: 2023/12/03 18:30:40 by pnguyen-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,16 @@
 
 char	*ft_strjoin(char const s1[], char const s2[])
 {
-	char	*str;
-	size_t	size;
+	char	*dest;
+	size_t	len1;
+	size_t	len2;
 
-	size = ft_strlen(s1) + ft_strlen(s2) + 1;
-	str = ft_calloc(size, sizeof(char));
-	if (str == 0)
+	len1 = ft_strlen(s1);
+	len2 = ft_strlen(s2);
+	dest = ft_calloc(len1 + len2 + 1, sizeof(char));
+	if (dest == 0)
 		return (0);
-	ft_strlcat(str, s1, size);
-	ft_strlcat(str, s2, size);
-	return (str);
+	ft_strlcpy(dest, s1, len1 + 1);
+	ft_strlcpy(dest + len1, s2, len1 + len2 + 1);
+	return (dest);
 }
