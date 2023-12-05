d778c6e4f98305cf294d7ba6cd73fdb786ede88f
Pascal <152603632+K-Pascal@users.noreply.github.com>
2023-12-05 20:10:03 +0100

    ft_substr() is now fully protected

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: pnguyen- <pnguyen-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/07 11:24:18 by pnguyen-          #+#    #+#             */
-/*   Updated: 2023/11/10 18:21:35 by pnguyen-         ###   ########.fr       */
+/*   Updated: 2023/11/10 18:42:11 by pnguyen-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,9 @@ char	*ft_substr(char const *s, unsigned int start, unsigned int len)
 	char			*str;
 	unsigned int	len_s;
 
-	len_s = ft_strlen(s);
 	if (s == NULL)
 		return (NULL);
+	len_s = ft_strlen(s);
 	if (start + len > len_s)
 		len = 0;
 	str = malloc((len + 1) * sizeof(char));
