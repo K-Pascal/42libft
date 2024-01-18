2ce77f6883ca27069961cf646fb768140ee08a39
Pascal <152603632+K-Pascal@users.noreply.github.com>
2024-01-18 13:49:32 +0100

    Fixed a small error that never occurs

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: pnguyen- <pnguyen-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/07 11:39:17 by pnguyen-          #+#    #+#             */
-/*   Updated: 2023/12/06 14:42:10 by pnguyen-         ###   ########.fr       */
+/*   Updated: 2024/01/18 13:48:44 by pnguyen-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,6 @@ char	*ft_strjoin(char const s1[], char const s2[])
 	if (!dest)
 		return (0);
 	ft_strlcpy(dest, s1, len1 + 1);
-	ft_strlcpy(dest + len1, s2, len1 + len2 + 1);
+	ft_strlcpy(dest + len1, s2, len2 + 1);
 	return (dest);
 }
