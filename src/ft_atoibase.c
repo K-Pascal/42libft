5e6012131ce9c1b5479727e8daf61671adab7eab
K-Pascal <152603632+K-Pascal@users.noreply.github.com>
2024-03-17 19:36:41 +0100

    add small changes without modifying the code

--- src/ft_atoibase.c
+++ src/ft_atoibase.c
@@ -6,7 +6,7 @@
 /*   By: pnguyen- <pnguyen-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/17 19:16:54 by pnguyen-          #+#    #+#             */
-/*   Updated: 2024/03/17 19:18:29 by pnguyen-         ###   ########.fr       */
+/*   Updated: 2024/03/17 19:30:22 by pnguyen-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,7 @@ static int	get_basepos(char const base[], char c)
 	char const	*found = ft_strchr(base, c);
 	if (found == NULL)
 		return (-1);
+
 	return (found - base);
 }
 
@@ -27,6 +28,7 @@ int	ft_atoibase(char const str[], char const base[])
 {
 	while (ft_isspace(*str))
 		str++;
+
 	int sign = 1;
 	if (*str == '+' || *str == '-')
 	{
@@ -34,8 +36,10 @@ int	ft_atoibase(char const str[], char const base[])
 			sign = -1;
 		str++;
 	}
+
 	if (*str == '\0')
 		return (0);
+
 	int	len_base = ft_strlen(base);
 	int	nb = 0;
 	int	i = get_basepos(base, *(str++));
